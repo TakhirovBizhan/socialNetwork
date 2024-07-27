679c39bbb574a82a5d36f16fb8828f400b88aa7a
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2024-07-27 23:09:25 +0300

    new: added header and static files, but it does not working. I might need to change it somehow.

---
+++ templates/friends.php
@@ -0,0 +1,6 @@
+<?php require(dirname(__DIR__).'\templates\header.php');?>
+
+?>
+
+
+<h1>this is friends section</h1>
---
+++ templates/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Социальная сеть</title>
+    <link rel="stylesheet" href="/static/style.css">
+</head>
+<body>
+<header>
+    <div class="header__wrapper">
+        <nav>
+            <ul class="nav__list">
+                <li><a href="">Сообщения</a></li>
+                <li><a href="friends.php">Друзья</a></li>
+                <li><a href="">Лента</a></li>
+                <li><a href="">Мои лайки</a></li>
+                <li><a href="">Настройки</a></li>
+                <li><a href="">Выйти</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
